4015f89cef677333d4415671820d3b43b92a9be4
Maël Naccache <contact@maeln.com>
2025-09-23 20:11:36 +0200

    exp: use opencv for the EDT.

--- mkbubble/src/main.rs
+++ mkbubble/src/main.rs
@@ -1,17 +1,47 @@
+use bpaf::{Parser, construct, positional, short};
+use image::{GenericImageView, ImageBuffer, Rgb, RgbImage};
+use indexmap::set::IndexSet;
+use opencv::{
+    core::{CV_8UC1, CV_32F, Mat, MatTrait, Point, Scalar, min_max_loc, no_array},
+    imgproc,
+};
 use std::{
     collections::{BTreeSet, HashSet},
     fmt::Display,
     path::PathBuf,
+    sync::Arc,
 };
 
-use bpaf::{Parser, construct, positional, short};
-use image::{DynamicImage, GenericImageView, ImageBuffer, Rgb, RgbImage};
-use rand::rngs::StdRng;
-use rand::seq::IteratorRandom;
-use rand::{Rng, SeedableRng};
-use rayon::iter::{IntoParallelRefIterator, ParallelBridge, ParallelIterator};
-
 const MAX_RADIUS: u32 = 25;
+
+#[derive(Clone, Debug, Hash)]
+struct Pixel {
+    x: u32,
+    y: u32,
+}
+
+impl PartialEq for Pixel {
+    fn eq(&self, other: &Self) -> bool {
+        self.x == other.x && self.y == other.y
+    }
+}
+impl PartialOrd for Pixel {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        match self.x.partial_cmp(&other.x) {
+            Some(std::cmp::Ordering::Equal) => self.y.partial_cmp(&other.y),
+            other => other,
+        }
+    }
+}
+impl Eq for Pixel {}
+impl Ord for Pixel {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        match self.x.cmp(&other.x) {
+            std::cmp::Ordering::Equal => self.y.cmp(&other.y),
+            other => other,
+        }
+    }
+}
 
 #[derive(Debug, Clone)]
 struct Circle {
@@ -19,38 +49,10 @@ struct Circle {
     y: u32,
     r: u32,
 }
-
 impl Display for Circle {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.write_str(&format!("[{},{},{}]", self.x, self.y, self.r))
     }
-}
-
-fn calculate_luminance(r: u8, g: u8, b: u8) -> f32 {
-    // Using the standard luminance formula: Y = 0.299*R + 0.587*G + 0.114*B
-    let r_norm = r as f32 / 255.0;
-    let g_norm = g as f32 / 255.0;
-    let b_norm = b as f32 / 255.0;
-    0.299 * r_norm + 0.587 * g_norm + 0.114 * b_norm
-}
-
-fn debug_img(
-    pixel_set: &HashSet<(u32, u32)>,
-    width: u32,
-    height: u32,
-    filename: &str,
-) -> Result<(), Box<dyn std::error::Error>> {
-    let mut img: RgbImage = ImageBuffer::new(width, height);
-    for pixel in img.pixels_mut() {
-        *pixel = Rgb([0, 0, 0]); // Black color
-    }
-    for &(x, y) in pixel_set {
-        if x < width && y < height {
-            img.put_pixel(x, y, Rgb([255, 255, 255]));
-        }
-    }
-    img.save(filename)?;
-    Ok(())
 }
 
 struct Args {
@@ -67,47 +69,96 @@ fn parse_args() -> Args {
     construct!(Args { path, debug }).to_options().run()
 }
 
+fn compute_edt(mask: &IndexSet<Pixel>, width: i32, height: i32) -> Option<Pixel> {
+    let mut binary_image =
+        Mat::new_rows_cols_with_default(height, width, CV_8UC1, Scalar::all(0.0)).unwrap();
+    for px in mask.iter() {
+        if let Ok(pixel) = binary_image.at_2d_mut::<u8>(px.y as i32, px.x as i32) {
+            *pixel = 255;
+        }
+    }
+    let mut dist = Mat::default();
+    if let Err(e) = imgproc::distance_transform(
+        &binary_image,
+        &mut dist,
+        imgproc::DIST_L2,
+        imgproc::DIST_MASK_PRECISE,
+        CV_32F,
+    ) {
+        eprintln!("could not compute edt: {}", e);
+    }
+
+    let mut min_val = 0.0;
+    let mut max_val = 0.0;
+    let mut min_loc = Point::default();
+    let mut max_loc = Point::default();
+    if let Err(e) = min_max_loc(
+        &dist,
+        Some(&mut min_val),
+        Some(&mut max_val),
+        Some(&mut min_loc),
+        Some(&mut max_loc),
+        &no_array(),
+    ) {
+        eprintln!("could not find min/max in edt: {}", e);
+    }
+
+    Some(Pixel {
+        x: max_loc.x as u32,
+        y: max_loc.y as u32,
+    })
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = parse_args();
+    opencv::core::set_num_threads(1)?;
     let img = image::open(&args.path)?;
-
-    let targets_pixels: HashSet<(u32, u32)> = (0..(img.width() * img.height()))
-        .map(|d1| (d1 % img.width(), d1 / img.width()))
-        .par_bridge()
+    let targets_pixels: IndexSet<Pixel> = (0..(img.width() * img.height()))
+        .map(|d1| Pixel {
+            x: d1 % img.width(),
+            y: d1 / img.width(),
+        })
         .filter(|coord| {
-            let pixel = img.get_pixel(coord.0, coord.1).0;
+            let pixel = img.get_pixel(coord.x, coord.y).0;
             let luminance = calculate_luminance(pixel[0], pixel[1], pixel[2]);
             return luminance > 0.5;
         })
         .collect();
 
     if args.debug {
-        debug_img(&targets_pixels, img.width(), img.height(), "targets.bmp")
+        debug_img(&targets_pixels, img.width(), img.height(), "targets.png")
             .expect("could not save debug img");
     }
 
     let mut circles: Vec<Circle> = Vec::new();
-    let mut first_pass_pixels = targets_pixels.clone();
-    let mut second_pass_pixels = targets_pixels.clone();
     let mut mask = targets_pixels.clone();
+    // let mut all_pixels_for_transform = Vec::with_capacity((img.width() * img.height()) as usize);
+    // let mut max_v: Option<u32> = None;
 
-    // First pass: We only care about fitting as many big circle as possible
-    while first_pass_pixels.len() > 0 {
-        let coord = first_pass_pixels.iter().next().unwrap().clone();
-        first_pass_pixels.remove(&coord);
-        let circle = find_biggest_circle(&mask, coord.0, coord.1, MAX_RADIUS);
-        if circle.r < 2 {
-            continue;
-        }
-        let to_rm = pixels_in_circle(circle.x, circle.y, circle.r);
-        circles.push(circle);
-        for px in to_rm {
-            mask.remove(&px);
+    while mask.len() > 0 {
+        if let Some(max_point) = compute_edt(&mask, img.width() as i32, img.height() as i32) {
+            let circle = find_biggest_circle(&mask, max_point.x, max_point.y, MAX_RADIUS);
+            if circle.r < 3 {
+                mask.swap_remove(&Pixel {
+                    x: max_point.x,
+                    y: max_point.y,
+                });
+                continue;
+            }
+            circles.push(circle.clone());
+
+            // 5. Remove the pixels of this new circle from the mask and repeat
+            let to_rm = pixels_in_circle(circle.x, circle.y, circle.r);
+            for px in to_rm {
+                mask.swap_remove(&px);
+            }
+        } else {
+            break;
         }
     }
 
     if args.debug {
-        let circle_px: HashSet<(u32, u32)> = circles
+        let circle_px: IndexSet<Pixel> = circles
             .iter()
             .map(|e| pixels_in_circle(e.x, e.y, e.r))
             .reduce(|mut acc, e| {
@@ -117,7 +168,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             .unwrap()
             .into_iter()
             .collect();
-        debug_img(&circle_px, img.width(), img.height(), "fist_pass.bmp")
+        debug_img(&circle_px, img.width(), img.height(), "fist_pass.png")
             .expect("could not save debug img");
     }
 
@@ -149,7 +200,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // }
 
     if args.debug {
-        let circle_px: HashSet<(u32, u32)> = circles
+        let circle_px: IndexSet<Pixel> = circles
             .iter()
             .map(|e| pixels_in_circle(e.x, e.y, e.r))
             .reduce(|mut acc, e| {
@@ -159,7 +210,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             .unwrap()
             .into_iter()
             .collect();
-        debug_img(&circle_px, img.width(), img.height(), "output.bmp")
+        debug_img(&circle_px, img.width(), img.height(), "output.png")
             .expect("could not save debug img");
     }
 
@@ -180,15 +231,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-fn find_biggest_circle(
-    valid_px: &HashSet<(u32, u32)>,
-    cx: u32,
-    cy: u32,
-    max_radius: u32,
-) -> Circle {
+fn find_biggest_circle(valid_px: &IndexSet<Pixel>, cx: u32, cy: u32, max_radius: u32) -> Circle {
     let mut circle = Circle { x: cx, y: cy, r: 1 };
     for r in 3..=max_radius {
         let valid = check_circle(valid_px, cx, cy, r);
+        // eprintln!("val: {} {}", valid, r);
         if !valid {
             break;
         }
@@ -197,7 +244,7 @@ fn find_biggest_circle(
     circle
 }
 
-fn pixels_in_circle(cx: u32, cy: u32, r: u32) -> Vec<(u32, u32)> {
+fn pixels_in_circle(cx: u32, cy: u32, r: u32) -> Vec<Pixel> {
     let mut pixels = Vec::new();
     let cx = cx as i32;
     let cy = cy as i32;
@@ -211,14 +258,17 @@ fn pixels_in_circle(cx: u32, cy: u32, r: u32) -> Vec<(u32, u32)> {
                     // We ignore out of bound pixels
                     continue;
                 }
-                pixels.push((x as u32, y as u32));
+                pixels.push(Pixel {
+                    x: x as u32,
+                    y: y as u32,
+                });
             }
         }
     }
     pixels
 }
 
-fn check_circle(valid_px: &HashSet<(u32, u32)>, cx: u32, cy: u32, radius: u32) -> bool {
+fn check_circle(valid_px: &IndexSet<Pixel>, cx: u32, cy: u32, radius: u32) -> bool {
     let cx = cx as i32;
     let cy = cy as i32;
     let radius = radius as i32;
@@ -231,7 +281,11 @@ fn check_circle(valid_px: &HashSet<(u32, u32)>, cx: u32, cy: u32, radius: u32) -
                     // We ignore out of bound pixels
                     continue;
                 }
-                if !valid_px.contains(&(x as u32, y as u32)) {
+                let px = Pixel {
+                    x: x as u32,
+                    y: y as u32,
+                };
+                if !valid_px.contains(&px) {
                     return false;
                 }
             }
@@ -239,4 +293,31 @@ fn check_circle(valid_px: &HashSet<(u32, u32)>, cx: u32, cy: u32, radius: u32) -
     }
 
     true
+}
+
+fn calculate_luminance(r: u8, g: u8, b: u8) -> f32 {
+    // Using the standard luminance formula: Y = 0.299*R + 0.587*G + 0.114*B
+    let r_norm = r as f32 / 255.0;
+    let g_norm = g as f32 / 255.0;
+    let b_norm = b as f32 / 255.0;
+    0.299 * r_norm + 0.587 * g_norm + 0.114 * b_norm
+}
+
+fn debug_img(
+    pixel_set: &IndexSet<Pixel>,
+    width: u32,
+    height: u32,
+    filename: &str,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let mut img: RgbImage = ImageBuffer::new(width, height);
+    for pixel in img.pixels_mut() {
+        *pixel = Rgb([0, 0, 0]); // Black color
+    }
+    for p in pixel_set {
+        if p.x < width && p.y < height {
+            img.put_pixel(p.x, p.y, Rgb([255, 255, 255]));
+        }
+    }
+    img.save(filename)?;
+    Ok(())
 }
